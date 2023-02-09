2b40a344a3430d07b708702cd144399a783655fd
AngeloTripodi <tripodi.angelo@gmail.com>
2023-02-09 18:07:47 +0100

    add partials and layouts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,45 +1,22 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+        @extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+        @section('main-content')
+            <div class="container my-5">
+                <div class="row row-cols-2 row-cols-md-3 row-cols-lg-5 g-4 mb-5">
 
-    <title>Home</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
-<body>
-
-
-    <main>
-        <div class="container my-5">
-            <div class="row row-cols-2 row-cols-md-3 row-cols-lg-5 g-4 mb-5">
-
-                @foreach ($movies as $movie)
-                    <div class="col">
-                        <div class="card text-bg-dark mb-3 h-100 text-center" style="max-width: 18rem;">
-                            <div class="card-header">{{ $movie->date }} || Vote: {{ $movie->vote }} </div>
-                            <div class="card-body">
-                                <h2 class="card-title">{{ $movie->title }}</h2>
-                                <h5>{{ $movie->original_title }}</h5>
-                                <p class="card-text">{{ $movie->nationality }}</p>
+                    @foreach ($movies as $movie)
+                        <div class="col">
+                            <div class="card text-bg-dark mb-3 h-100 text-center" style="max-width: 18rem;">
+                                <div class="card-header">{{ $movie->date }} || Vote: {{ $movie->vote }} </div>
+                                <div class="card-body">
+                                    <h2 class="card-title">{{ $movie->title }}</h2>
+                                    <h5>{{ $movie->original_title }}</h5>
+                                    <p class="card-text">{{ $movie->nationality }}</p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                @endforeach
+                    @endforeach
 
+                </div>
             </div>
-        </div>
-
-    </main>
-
-</body>
-
-</html>
+        @endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,13 @@
+<header>
+    <nav class="navbar bg-dark navbar-dark navbar-expand-lg bg-primary">
+        <div class="container-fluid">
+            <ul class="navbar-nav align-items-center flex justify-content-between w-100">
+                <li>
+                    <h3 class="text-white">Laravel Model Controller</h3>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="{{ route('home') }}">HOMEPAGE</a>
+                </li>
+        </div>
+    </nav>
+</header>
